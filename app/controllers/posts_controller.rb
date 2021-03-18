7ae633fbf599a91d8060778bba82ac2002506443
suzuki-gunji <gunji1102@gmail.com>
2021-03-18 18:44:41 +0900

    画像に関するアソシエーションの記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  private
+
+  def message_params
+    params.require(:post).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :users
   has_many :comments
+  has_one_attached :image
 end
