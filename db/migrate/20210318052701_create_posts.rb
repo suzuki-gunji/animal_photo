a8ad0dd997f3d02d13e2a4d471a592120a06145b
suzuki-gunji <gunji1102@gmail.com>
2021-03-18 14:54:33 +0900

    マイグレーションファイルの作成

--- db/migrate/20210318052701_create_posts.rb
+++ db/migrate/20210318052701_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title,               null:false
+      t.integer :animal_type_id,     null: false
+      t.references :user,            foreign_key: true
       t.timestamps
     end
   end
