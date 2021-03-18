dbb70a7773dc67f005ee51164edf61d120c2bdbe
suzuki-gunji <gunji1102@gmail.com>
2021-03-18 19:38:26 +0900

    新規投稿の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new 
+    end  
+  end
+
   private
 
-  def message_params
-    params.require(:post).permit(:image).merge(user_id: current_user.id)
+  def post_params
+    params.require(:post).permit(:image, :title).merge(user_id: current_user.id)
   end
 end
