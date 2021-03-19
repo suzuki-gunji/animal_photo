525dc80dcf05294e753d2813311a44f48ea7985b
suzuki-gunji <gunji1102@gmail.com>
2021-03-19 19:16:57 +0900

    ユーザー情報編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
     @nickname = current_user.id
     @posts = current_user.posts
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if current_user == @user
+      if @user.update(user_params)
+        redirect_to user_path(current_user.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :profile)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :posts
-  resources :users, only: :show
-
+  resources :users, only: [:show, :edit, :update]
 end
